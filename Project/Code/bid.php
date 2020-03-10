4639acf97b2e5533d3ef3ceecc3a27246f1cc940
Yisumic <35821379+Yisumic@users.noreply.github.com>
2020-03-10 02:23:13 -0600

    Add files via upload

--- Project/Code/bid.php
+++ Project/Code/bid.php
@@ -1,58 +1,69 @@
 <?php
+
+/**
+ * This file is using to check the hightest bider,
+ * and automatically assign the item and update the 
+ * BP for the particular user.
+ */
+
+// connect to database
 $con = new mysqli("localhost", "gaoha202", "project", "gaoha202");
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
+// check the auction status : -2-> on sale; 1-> sold
 $now_date = date("Y-m-d");
-$tmp_sql = "SELECT * FROM History WHERE auction_status = -2 and auction_end_date < '$now_date'";
-$list = $con->query($tmp_sql);
+$sql = "SELECT * FROM History WHERE auction_status = -2 and auction_end_date < '$now_date'";
+$list = $con->query($sql);
 
-while ( $row = $list->fetch_assoc() ) {
-    $tmp_battle_id = $row['battle_id'];
-    $tmp_uid = $row['uid'];
-    $tmp_auction_end_date = $row['auction_end_date']." 23:59:59";
-    
-    
-    $tmp_sql = "SELECT * FROM (
-          SELECT * FROM Auction WHERE battle_id = '$tmp_battle_id'
-          and date <= '$tmp_auction_end_date' order by bp desc limit 10000 ) AS tmp group by item_id";
-    $tmp_list = $con->query($tmp_sql);
-    
-    while ( $tmp_row = $tmp_list->fetch_assoc() ) {
-        $tmp_dropped_id = $tmp_row['item_id'];
-        $tmp_sql = "SELECT * FROM Dropped WHERE item_id = '$tmp_dropped_id'";
-        $tmp_result = $con->query($tmp_sql);
-        $tmp_dropped_info  = $tmp_result->fetch_assoc();
-        if( !$tmp_dropped_info ){
+// Auction system
+while ($row = $list->fetch_assoc()) {
+    $battle_id = $row['battle_id'];
+    $uid = $row['uid'];
+    $auction_end_date = $row['auction_end_date'] . " 23:59:59";
+
+    // Find the highest bider.
+    $sql = "SELECT * FROM (
+          SELECT * FROM Auction WHERE battle_id = '$battle_id'
+          and date <= '$auction_end_date' order by bp desc limit 10000 ) AS tmp group by item_id";
+    $list = $con->query($sql);
+
+    // Searching from the dropped trable
+    while ($row = $list->fetch_assoc()) {
+        $dropped_id = $row['item_id'];
+        $sql = "SELECT * FROM Dropped WHERE item_id = '$dropped_id'";
+        $result = $con->query($sql);
+        $dropped_info = $result->fetch_assoc();
+        if (! $dropped_info) {
             continue;
         }
-        $tmp_bid_bp = $tmp_row['bp'];
-       
-       
-        $tmp_name = $tmp_row['members_name'];
-        $tmp_uid = $tmp_row['uid'];
-        $tmp_item = $tmp_dropped_info['item_name'];
-        $tmp_sql = "INSERT INTO Obtained ('uname','item','uid',)  VALUES ('$tmp_name','$tmp_item','$tmp_uid')";
-        $con->query( $tmp_sql );
+        $bid_bp = $row['bp'];
 
-       
-        $tmp_sql = "SELECT * FROM Performance WHERE battle_id = {$tmp_battle_id}";
-        $tmp_result = $con->query($tmp_sql);
-        
-        while ( $tmp_performance_row = $tmp_result->fetch_assoc() ){
-           
-            $tmp_member_name = $tmp_performance_row['members_name'];
-            $tmp_sql = "UPDATE Members SET BP = BP - '$tmp_bid_bp'  WHERE uname = '$tmp_member_name' and uid = '$tmp_uid'";
-            $con->query( $tmp_sql );
+        // Assign the item
+        $name = $row['members_name'];
+        $uid = $row['uid'];
+        $item = $dropped_info['item_name'];
+        $sql = "INSERT INTO Obtained ('uname','item','uid',)  VALUES ('$name','$item','$uid')";
+        $con->query($sql);
+
+        //Update the BP
+        $sql = "SELECT * FROM Performance WHERE battle_id = {$battle_id}";
+        $result = $con->query($sql);
+        while ($performance_row = $result->fetch_assoc()) {
+
+            $member_name = $performance_row['members_name'];
+            $sql = "UPDATE Members SET BP = BP - '$bid_bp'  WHERE uname = '$member_name' and uid = '$uid'";
+            $con->query($sql);
+            
         }
 
-       
-        $tmp_sql = "UPDATE Auction SET status = 1 WHERE id = ".$tmp_row['id'];
-        $con->query( $tmp_sql );
-      
-        $tmp_sql = "UPDATE History SET auction_status = 1 WHERE battle_id = '$tmp_battle_id'";
-        $con->query( $tmp_sql );
+        //Update auction status
+        $sql = "UPDATE Auction SET status = 1 WHERE id = " . $row['id'];
+        $con->query($sql);
+        $sql = "UPDATE History SET auction_status = 1 WHERE battle_id = '$battle_id'";
+        $con->query($sql);
     }
 }
+echo "Staring Working...."
 ?>
--- Project/Code/itemAuction.php
+++ Project/Code/itemAuction.php
@@ -36,16 +36,14 @@ if (isset($_POST["bid"])) {
     $battle_id = $dropped_info['battle_id'];
     $uid = $dropped_info['uid'];
 
-    if ($mname==""||$job==""||$mbp == "") {
+    if ($mname == "" || $job == "" || $mbp == "") {
         $error = "Input cannot be empty!";
     } else {
         $error = "";
         $add = "INSERT INTO Auction(item_id, members_name, job,bp, date,battle_id,uid) VALUES ('$item_id', '$mname','$job','$mbp',NOW(),'$battle_id','$uid')";
         $added = $con->query($add);
         header("location:itemAuction.php?item_id=$item_id");
-       
     }
-  
 }
 
 $bid = "select * from Auction WHERE item_id = '$item_id' LIMIT 8";
@@ -57,10 +55,10 @@ $bids = $con->query($bid);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="rsmMain.css">
+<link rel="stylesheet" href="style.css">
 <link rel="stylesheet" type="text/css" href="error.css" />
 
-<title>MainPage</title>
+<title>Auction Page</title>
 </head>
 <body>
 	<header></header>
@@ -68,65 +66,98 @@ $bids = $con->query($bid);
 		<ul>
 			<li class="nav-item"><a href="mainpage.php">Home</a></li>
 			<li class="nav-item"><a href="newRecord.php">Add New Record</a></li>
-			<li class="nav-item"><a href="raidRecord.php">Raid Record </a></li>
+			<li class="nav-item"><a href="raidRecord.php">Raid Record</a></li>
 			<li class="nav-item"><a href="memberDetail.php">Members Detail</a></li>
 			<li class="nav-item"><a href="memberChange.php">Add/Remove Members</a></li>
 		</ul>
 		<ul class="fr">
-			<li class="nav-item"><?php echo "Welcome ! Dear User ";  echo $username;?></li>
+			<li class="nav-item">
+				<?php echo "Welcome ! Dear User ";echo $username; ?>
+			</li>
 			<li class="nav-item"><a href="rsmLogout.php">Log out</a></li>
 		</ul>
 	</nav>
-	<main class="clearfix">
+	<!-- main -->
+	<main>
+	<table style="margin-bottom: 10px">
+		<tbody>
+			<tr>
+				<td width="25%"><img style="height: 600px"
+					src="artResources/character1.jpg" alt="" class="img" /></td>
+				<td width="50%">
+					<div class="history-box">
+						<div class="title cl">
+							<h3>Make a Bid</h3>
+						</div>
+						<div style="padding-bottom: 10px">
 
-		<div id="auctions">
-			<p>Auction : <?=$dropped_info['item_name'];?></p>
-			<form class="rsmadd" action="itemAuction.php?item_id=<?=$item_id;?>"
-				method="post" enctype="multipart/form-data">
-				<tr>
-					<p class="err_msg"><?php echo "$error";?></p>
-				</tr>
-				<tr>
-					<td>Member Name (full):</td>
-					<td><input class="right_msg" type="text" name="mname" size="20" /></td>
-					<td>Job:</td>
-					<td><input class="right_msg" type="text" name="job" size="5" /></td>
-					<td>BP:</td>
-					<td><input class="right_msg" type="text" name="mbp" size="1" /></td>
-					<td></td>
-					<td><input class="add" type="submit" name="bid" value="Bid" /></td>
+							<h2>Item Name : <?=$dropped_info['item_name'];?></h2>
+							<form class="rsmadd" action="itemAuction.php?item_id=<?=$item_id;?>" method="post" enctype="multipart/form-data">
+								<table>
+									<tbody>
+								<tr>
+									<p class="err_msg"><?php echo "$error";?></p>
+								</tr>
+								<tr>
+									<td>Member Name (full):</td>
+									<td><input class="right_msg" type="text" name="mname" size="20" /></td>
+									<td>Job:</td>
+									<td><input class="right_msg" type="text" name="job" size="5" /></td>
+									<td>Bid(BP):</td>
+									<td><input class="right_msg" type="text" name="mbp" size="1" /></td>
+									<td></td>
+									<td><input class="btn" type="submit" name="bid" value="Submit" /></td>
 
-				</tr>
-			</form>
-			<table>
-				<tr>
-					<td><p>Member Name</p></td>
-					<td><p>Job</p></td>
-					<td><p>BP</p></td>
-				</tr>
-				 <?php
+								</tr>
+									</tbody>
+								</table>
+							</form>
+						</div>
+
+						<div class="history-table">
+							<div class="table-head">
+								<table width="100%" border="0" cellpadding="0" cellspacing="0">
+									<tbody>
+										<tr class="bt_ph">
+											<td><p>Member Name</p></td>
+											<td width="25%"><p>Job</p></td>
+											<td width="15%"><p>BP</p></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+
+							<div class="table-body progress_scroll" style="height: 450px">
+								<table style="border-collapse: collapse;" width="100%"
+									border="0" cellpadding="0" cellspacing="0">
+									<tbody>		
+				 	<?php
     while ($list = $bids->fetch_assoc()) {
-        ?><tr>
+        ?>								<tr>
 
-					<td><?=$list["members_name"]?></td>
-					<td><p><?=$list["job"]?></p></td>
-					<td><p><?=$list["bp"]?></p></td>
-				</tr>
+											<td><?=$list["members_name"]?></td>
+											<td width="25%"><p><?=$list["job"]?></p></td>
+											<td width="15%"><p><?=$list["bp"]?></p></td>
+										</tr>
                                     <?php }?>
                                     
-			</table>
-
-			<hr />
-            
-<?php
-
-$con->close();
-?>
-</div>
-	</section>
-	<hr />
-
-	<footer> @copy right @gaoha202 </footer>
-
+			
+								
+								
+                                </tbody>
+								</table>
+ 							</div>
+						</div>
+					</div>
+				</td>
+				<td width="25%"><img style="height: 600px"
+					src="artResources/character2.jpg" alt="" class="img fr" /></td>
+			</tr>
+		</tbody>
+	</table>
+	<?php
+            $con->close();
+                    ?>
+	<footer>&copy;gaoha202@uregina.ca</footer> </main>
 </body>
 </html>
